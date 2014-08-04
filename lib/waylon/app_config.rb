b0a4abbaf3103b6593c55f27a6912c336b9d0a20
Adrien Thebo <git@somethingsinistral.net>
2014-08-04 12:37:29 -0700

    Update app_config rebuild_interval to match waylon.js interval

--- lib/waylon/app_config.rb
+++ lib/waylon/app_config.rb
@@ -7,7 +7,7 @@ class Waylon
     def self.from_hash(hash)
       new.tap do |x|
         x.refresh_interval = (hash['refresh_interval'] || 60)
-        x.rebuild_interval = (hash['rebuild_interval'] || 600)
+        x.rebuild_interval = (hash['rebuild_interval'] || 3600)
       end
     end
   end
