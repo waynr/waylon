e8300b72a703ca6ba16551d05792e36286587d89
Roger Ignazio <roger@puppetlabs.com>
2014-09-26 20:17:31 -0700

    WIP: usability enhancements for trouble state

--- public/js/waylon/view/trouble.js
+++ public/js/waylon/view/trouble.js
@@ -19,12 +19,13 @@ Waylon.Views.Trouble = Backbone.View.extend({
         var failed     = parseInt(Waylon.JobCollection.count('failure'));
         var total      = parseInt(Waylon.JobCollection.size());
         var threshold  = parseInt(this.trouble_threshold);
+        var hashstring = window.location.hash;
 
         if (successful === total) {
             // Don't do anything, this is Nirvana mode
         } else {
             // Trouble mode is disabled if threshold is 0 (default)
-            if (threshold > 0) {
+            if (threshold > 0 && hashstring != '#disable-trouble') {
                 if (failed >= threshold) {
                     this.begin();
                 } else {
@@ -39,12 +40,28 @@ Waylon.Views.Trouble = Backbone.View.extend({
         this.$el.css('background-image', 'url(' + this.imageOfTheDay() + ')');
 
         this.radiator.hide();
+
+        var theTemplate = [
+            '<div id="trouble-mode-alert" class="alert alert-danger alert-dismissable" role="alert">',
+            '<button type="button" class="close" data-dismiss="alert">',
+            '<span aria-hidden="true">&times;</span>',
+            '<span class="sr-only">Close</span></button>',
+            '<strong>Heads up!</strong> This view has entered "trouble" ',
+            'state. This means the number of failed jobs is greater than ',
+            '<code>trouble_threshold</code> as configured in waylon.yml. ',
+            'If you need to see the radiator view of jobs, ',
+            '<a class="alert-link" href="#disable-trouble">click here</a> ',
+            'to return to the list of jobs.',
+            '</div>',
+            ].join("");
+        $('#global-alerts').html(theTemplate);
     },
 
     end: function() {
         this.$el.removeClass('trouble');
         this.$el.css('background-image', 'none');
 
+        $('#global-alerts').remove('#trouble-mode-alert');
         this.radiator.show();
     },
 
